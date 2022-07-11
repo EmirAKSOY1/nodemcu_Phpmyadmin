8595fc4f94df81f4783c3439dc73b1b1ebe01d50
EmirAKSOY1 <72320002+EmirAKSOY1@users.noreply.github.com>
2022-07-11 16:13:27 +0300

    Add files via upload

---
+++ nodemcu_php_myadmin.ino
@@ -0,0 +1,82 @@
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
+
+const char* ssid     = "EA";
+const char* password = "emr954elf654ADZ";
+const char* host = "192.168.1.104";
+
+
+void setup()
+{
+  
+    Serial.begin(115200);
+
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+
+
+void loop()
+{
+  
+
+   
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort =80;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
+ 
+
+
+    // This will send the request to the server
+ client.print(String("GET http://your_hostname/connect.php?") + 
+                          ("&temperature=") + 15 +
+                          ("&humidity=") + 20+
+                          " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 1000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+        
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
+}
